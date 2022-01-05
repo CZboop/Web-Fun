9c6af98d18c8dda90b024ea0ad3f2ed05d390f0e
Branchey <70140890+CZboop@users.noreply.github.com>
2022-01-05 12:41:15 +0000

    changed blood splatter to have a slight timeout and have more of a shape than randomly across page

--- blood_splatter/main.js
+++ blood_splatter/main.js
@@ -1,14 +1,11 @@
-// will execute the main function on mouse move
 document.addEventListener("mousemove", para);
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // creating svg circles semi randomly
-    var svgCircle = document.getElementById("red-circles");
-    var NS = "http://www.w3.org/2000/svg";
-    for (var i = 0; i < 400; i++) {
+// function makes circles randomly across the svg viewbox
+const makeCircle = (i,NS,svgCircle) => {
       var circlex = Math.floor(Math.random()*50);
       var circley = Math.floor(Math.random()*50);
       var circle = document.createElementNS(NS, "circle");
+      console.log(circle);
       circle.setAttribute("cx", circlex);
       circle.setAttribute("cy", circley );
       circle.setAttribute("r", Math.random()/5);
@@ -16,11 +13,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
       circle.setAttribute("data-speed", Math.floor(Math.random()*10));
       circle.setAttribute("class", "circle-drawn")
       svgCircle.appendChild(circle);
-    };
+    
+
+}
+
+// function makes circles in more of a streak
+const makeCirclesInLine = (i,NS,svgCircle) => {
+    var circlex = 50%i;
+    var circley = 50%(50/i)+Math.round(Math.random()*5)+2;
+    var circle = document.createElementNS(NS, "circle");
+    console.log(circle);
+    circle.setAttribute("cx", circlex);
+    circle.setAttribute("cy", circley );
+    circle.setAttribute("r", Math.random()/5);
+    circle.setAttribute("fill", "red");
+    circle.setAttribute("data-speed", Math.floor(Math.random()*5));
+    circle.setAttribute("class", "circle-drawn")
+    svgCircle.appendChild(circle);
+  
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var svgCircle = document.getElementById("red-circles");
+    var NS = "http://www.w3.org/2000/svg";
+    for (let i = 0; i < 200; i++) {
+        setTimeout(() => {makeCirclesInLine(i,NS,svgCircle)}, i*5);
+        setTimeout(() => {makeCirclesInLine(i,NS,svgCircle)}, i*5);
+    }
+    
   });
 
 function para(e){
-    // adding parallax proportional to data speed attribute randomly assigned to each circle made above
+    // 
     this.querySelectorAll('.circle-drawn').forEach(layer => {
         const level = layer.getAttribute('data-speed');
 
@@ -28,20 +53,5 @@ function para(e){
         const y = (window.innerHeight - e.pageY*level)/200;
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    })
-    // making the pupils follow the mouse
-    this.querySelectorAll('.pupil1').forEach(pupil => {
-        const x = (window.innerWidth + e.pageX)/160-7.5;
-        const y = (window.innerHeight + e.pageY)/100-5;
-
-        pupil.setAttribute("cx", x);
-        pupil.setAttribute("cy", y);
-    })
-    this.querySelectorAll('.pupil2').forEach(pupil => {
-        const x = (window.innerWidth + e.pageX)/160+7.5;
-        const y = (window.innerHeight + e.pageY)/100-5;
-
-        pupil.setAttribute("cx", x);
-        pupil.setAttribute("cy", y);
     })
 }
